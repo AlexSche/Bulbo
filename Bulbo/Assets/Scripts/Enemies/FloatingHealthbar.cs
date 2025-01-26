6d849cd3daec1533e9bc875563e892d6bddba77d
AlexSche <Alexander.S_97@gmx.de>
2025-01-26 20:03:42 +0100

    add healthbar

---
+++ Bulbo/Assets/Scripts/Enemies/FloatingHealthbar.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FloatingHealthbar : MonoBehaviour
+{
+    private Slider slider;
+    private new Camera camera;
+
+
+    void Start() {
+        slider = GetComponent<Slider>();
+        camera = Camera.main;
+    }
+
+    void Update() {
+        transform.rotation = camera.transform.rotation;
+    }
+
+    public void UpdateHealthbar(float currentValue, float maxValue) {
+        slider.value = currentValue/maxValue;
+    }
+}
