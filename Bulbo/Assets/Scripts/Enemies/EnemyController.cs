fb980c3418c1c06eca03cb019cb7f1b311f26dc0
AlexSche <Alexander.S_97@gmx.de>
2025-01-26 17:48:04 +0100

    scale enemies

--- Bulbo/Assets/Scripts/Enemies/EnemyController.cs
+++ Bulbo/Assets/Scripts/Enemies/EnemyController.cs
@@ -15,7 +15,7 @@ public class EnemyController : MonoBehaviour
         {
             Vector2 moveDirection = targetPosition - transform.position;
             transform.LookAt(Vector3.zero);
-            transform.position = Vector3.Lerp(transform.position, targetPosition, 0.1f);
+            transform.position = Vector3.Lerp(transform.position, targetPosition, 0.05f);
             yield return new WaitForSeconds(1);
         }
     }
