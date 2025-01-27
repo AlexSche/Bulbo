813188bade583009f20f0d395a496a5e9773e459
AlexSche <Alexander.S_97@gmx.de>
2025-01-27 18:21:59 +0100

    small changes

--- Bulbo/Assets/Scripts/Bulbo/PowerUps/Lasereyes.cs
+++ Bulbo/Assets/Scripts/Bulbo/PowerUps/Lasereyes.cs
@@ -1,16 +1,18 @@
 using UnityEngine;
 
-public class Lasereyes : Gun
+public class Lasereyes : MonoBehaviour
 {
+    public Transform[] bulletSpawnPoint;
+    public GameObject LaserPrefab;
     [SerializeField] private PowerUpSO powerUpSO;
 
     public void shootAtPosition(Vector3 position)
     {
         Transform chosenSpawnPoint = bulletSpawnPoint[0].transform;
         chosenSpawnPoint.LookAt(position);
-        GameObject bullet = Instantiate(bulletPrefab, chosenSpawnPoint.position, chosenSpawnPoint.rotation);
+        GameObject bullet = Instantiate(LaserPrefab, chosenSpawnPoint.position, chosenSpawnPoint.rotation);
         chosenSpawnPoint = bulletSpawnPoint[1].transform;
         chosenSpawnPoint.LookAt(position);
-        GameObject bullet2 = Instantiate(bulletPrefab, chosenSpawnPoint.position, chosenSpawnPoint.rotation);
+        GameObject bullet2 = Instantiate(LaserPrefab, chosenSpawnPoint.position, chosenSpawnPoint.rotation);
     }
 }
