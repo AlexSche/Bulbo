5660b6da6cc934d447c6dd4a4ef354b533fdb640
AlexSche <Alexander.S_97@gmx.de>
2025-01-28 20:00:49 +0100

    reset doesnt work

--- Bulbo/Assets/Scripts/UI/MainMenu.cs
+++ Bulbo/Assets/Scripts/UI/MainMenu.cs
@@ -2,12 +2,9 @@ using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
-    private GameObject player;
     public void Awake()
     {
-        player = GameObject.FindWithTag("Player");
         Time.timeScale = 0;
-        player.SetActive(false);
         gameObject.SetActive(true);
     }
 
@@ -19,7 +16,6 @@ public class MainMenu : MonoBehaviour
     public void startGame()
     {
         gameObject.SetActive(false);
-        player.SetActive(true);
         Time.timeScale = 1;
     }
 
