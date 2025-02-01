09dd9ccc211ea995b277ea66fc46f34e3ad84655
AlexSche <Alexander.S_97@gmx.de>
2025-02-01 16:33:54 +0100

    add fullscreen damage effect

---
+++ Bulbo/Assets/Scripts/Environment/FullScreenEffectController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class FullScreenEffectController : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private ScriptableRendererFeature fullScreenDamage;
+    [SerializeField] private Material material;
+    [SerializeField] private PlayerControllerChannel playerControllerChannel;
+
+    private float fadeOutTime = 0.5f;
+    private int vignetteIntensity = Shader.PropertyToID("_VignetteIntensity");
+    private const float VIGNETTE_INTENSITY_START_AMOUNT = 1;
+
+    void Start() {
+        fullScreenDamage.SetActive(false);
+        playerControllerChannel.healthChanged += startEffect;
+    }
+
+    void startEffect(float maxValue, float currentValue) {
+        StartCoroutine(hurt());
+    }
+
+    IEnumerator hurt() {
+        fullScreenDamage.SetActive(true);
+        material.SetFloat(vignetteIntensity, VIGNETTE_INTENSITY_START_AMOUNT);
+        yield return new WaitForSeconds(0.3f);
+        float elapsedTime = 0f;
+        while (elapsedTime < fadeOutTime) {
+            elapsedTime += Time.deltaTime;
+            float lerpedVignette = Mathf.Lerp(VIGNETTE_INTENSITY_START_AMOUNT, 0f, elapsedTime / fadeOutTime);
+            material.SetFloat(vignetteIntensity, lerpedVignette);
+            yield return 0;
+        }
+        fullScreenDamage.SetActive(false);
+    }
+}
